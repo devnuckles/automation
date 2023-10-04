95b0325d523833c10b316e0bb511e446886e6223
Proshanto <proshantolal3@gmail.com>
2023-10-04 21:29:40 +0600

    fixes S3 objects public access

--- iac/S3/main.tf
+++ iac/S3/main.tf
@@ -2,18 +2,11 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = "dev-automation-client-features"
 }
 
-resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+resource "aws_s3_bucket_public_access_block" "access_block" {
   bucket = aws_s3_bucket.s3_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource ="${aws_s3_bucket.s3_bucket.arn}/*",
-      },
-    ],
-  })
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
 }
