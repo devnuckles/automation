bc7b7467adf729da8cfcaef7d1e12f9a143f8ac8
Rafi5734 <kamrulhasun5734@gmail.com>
2023-10-28 15:14:20 +0600

    login page redeign done

--- client/src/modules/platform/users/components/login.component.js
+++ client/src/modules/platform/users/components/login.component.js
@@ -1,156 +1,191 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
+
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
+
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
+
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import loginImage from "../../../utils/images&icons/login_right_imag.png";
 
-function Copyright(props) {
-    return (
-        <Typography
-            variant="body2"
-            color="text.secondary"
-            align="center"
-            {...props}
-        >
-            {"Copyright © "}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
-}
+import "../components/style/loginStyle.css";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  FormGroup,
+  InputAdornment,
+} from "@mui/material";
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
-export default function SignInSide() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
-    };
-
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Grid container component="main" sx={{ height: "100vh" }}>
-                <CssBaseline />
-
-                <Grid
-                    item
-                    xs={12}
-                    sm={8}
-                    md={5}
-                    component={Paper}
-                    elevation={6}
-                    square
-                >
-                    <Box
-                        sx={{
-                            my: 8,
-                            mx: 4,
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                        </Typography>
-                        <Box
-                            component="form"
-                            noValidate
-                            onSubmit={handleSubmit}
-                            sx={{ mt: 1 }}
-                        >
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        value="remember"
-                                        color="primary"
-                                    />
-                                }
-                                label="Remember me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                            <Copyright sx={{ mt: 5 }} />
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid
-                    item
-                    xs={false}
-                    sm={4}
-                    md={7}
-                    sx={{
-                        backgroundImage:
-                            "url(https://source.unsplash.com/random?wallpapers)",
-                        backgroundRepeat: "no-repeat",
-                        backgroundColor: (t) =>
-                            t.palette.mode === "light"
-                                ? t.palette.grey[50]
-                                : t.palette.grey[900],
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
+export default function LogInSide() {
+  return (
+    <div className="log_in_bg">
+      <div
+        // style={{
+        //   padding: "100px",
+        //   height: "100%",
+        // }}
+        className="login_wrapper"
+      >
+        <Grid container spacing={1}>
+          <Grid xs={12} sm={6} md={6} className="login_left">
+            <Card
+              sx={{
+                width: "100%",
+                borderRadius: "10px",
+                boxShadow: "0px 4px 20px 0px rgba(0, 0, 0, 0.35)",
+              }}
+            >
+              <CardContent
+                style={{
+                  paddingTop: "40px",
+                  width: "100%",
+                  paddingBottom: "60px",
+                }}
+                className="login_card_content"
+              >
+                <div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      alignItems: "center",
                     }}
-                />
-            </Grid>
-        </ThemeProvider>
-    );
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="51"
+                      height="45"
+                      viewBox="0 0 51 45"
+                      fill="none"
+                    >
+                      <path
+                        opacity="0.6"
+                        d="M29.6667 29.9998C29.6667 33.3186 28.0625 36.3186 25.5 38.3623C23.2917 40.1623 20.375 41.2498 17.1667 41.2498C10.2709 41.2498 4.66669 36.2061 4.66669 29.9998C4.66669 24.8248 8.58335 20.4373 13.875 19.1436C15.3125 22.4061 18.3959 24.9186 22.2084 25.8561C23.25 26.1186 24.3542 26.2498 25.5 26.2498C26.6459 26.2498 27.75 26.1186 28.7917 25.8561C29.3542 27.1311 29.6667 28.5373 29.6667 29.9998Z"
+                        fill="#1E638A"
+                      />
+                      <path
+                        d="M38 15C38 16.4625 37.6875 17.8687 37.125 19.1438C35.6875 22.4062 32.6042 24.9188 28.7917 25.8563C27.75 26.1187 26.6458 26.25 25.5 26.25C24.3542 26.25 23.25 26.1187 22.2083 25.8563C18.3958 24.9188 15.3125 22.4062 13.875 19.1438C13.3125 17.8687 13 16.4625 13 15C13 8.79375 18.6042 3.75 25.5 3.75C32.3958 3.75 38 8.79375 38 15Z"
+                        fill="#1E638A"
+                      />
+                      <path
+                        opacity="0.4"
+                        d="M46.3333 29.9998C46.3333 36.2061 40.7292 41.2498 33.8333 41.2498C30.625 41.2498 27.7083 40.1623 25.5 38.3623C28.0625 36.3186 29.6667 33.3186 29.6667 29.9998C29.6667 28.5373 29.3542 27.1311 28.7917 25.8561C32.6042 24.9186 35.6875 22.4061 37.125 19.1436C42.4167 20.4373 46.3333 24.8248 46.3333 29.9998Z"
+                        fill="#1E638A"
+                      />
+                    </svg>
+                    <Typography
+                      sx={{
+                        ml: "10px",
+                        mt: "5px",
+                        fontWeight: "bold",
+                        fontSize: "25px",
+                      }}
+                    >
+                      Automation
+                    </Typography>
+                  </div>
+                </div>
+
+                <Typography
+                  sx={{
+                    mb: 1.5,
+                    fontSize: "40px",
+                    fontWeight: "700",
+                    lineHeight: "100px",
+                  }}
+                >
+                  Login
+                </Typography>
+                <form style={{ width: "90%" }}>
+                  <div style={{ width: "100%" }}>
+                    <TextField
+                      sx={{ mb: "30px" }}
+                      fullWidth
+                      type="email"
+                      name="email"
+                      label="Email"
+                      id="fullWidth"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <EmailOutlinedIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                    <TextField
+                      fullWidth
+                      type="password"
+                      name="password"
+                      label="Password"
+                      id="fullWidth"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <LockOpenOutlinedIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+
+                    <FormGroup sm={{}}>
+                      <FormControlLabel
+                        sx={{ mt: "10px", fontWeight: "bold" }}
+                        control={
+                          <Checkbox
+                            sx={{
+                              color: "#00DB99",
+                              "&.Mui-checked": {
+                                color: "#00DB99",
+                              },
+                            }}
+                          />
+                        }
+                        label="Remember Me"
+                      />
+                    </FormGroup>
+                  </div>
+                  <CardActions
+                    sx={{
+                      width: "100%",
+                      margin: "0",
+                      padding: "0",
+                      mt: "20px",
+                    }}
+                  >
+                    <Button
+                      variant="contained"
+                      sx={{
+                        fontFamily: "Outfit",
+                        fontSize: "18px",
+                        fontWeight: "600",
+                        bgcolor: "#00DB99",
+                        "&:hover": {
+                          bgcolor: "#00DB99",
+                        },
+                        width: "100%",
+                      }}
+                    >
+                      Login
+                    </Button>
+                  </CardActions>
+                </form>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid xs={12} sm={6} md={6} className="login_right">
+            <div>
+              <img style={{ width: "100%" }} src={loginImage} alt="loginIcon" />
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
