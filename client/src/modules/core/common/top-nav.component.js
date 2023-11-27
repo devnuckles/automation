c23d4368515ea24124c8dc81e0a4670a91e4c49b
devnuckles <mohammadroman925@gmail.com>
2023-11-28 00:35:26 +0600

    Add the Functionality of Profile Dropdown

--- client/src/modules/core/common/top-nav.component.js
+++ client/src/modules/core/common/top-nav.component.js
@@ -1,4 +1,14 @@
+import React, { useState } from "react";
+import { ProfileDropdown } from "../../platform/users";
+
 export default function TopNav() {
+    const [isProfileDropdownVisible, setIsProfileDropdownVisible] =
+        useState(false);
+
+    const toggleProfileDropdown = () => {
+        setIsProfileDropdownVisible(!isProfileDropdownVisible);
+    };
+
     return (
         <div className="row dashboard-right-top-nav">
             <div className="col-lg-8 col-md-8 dashboard-right-heading">
@@ -10,13 +20,17 @@ export default function TopNav() {
                     <i className="bi bi-bell me-3"></i>
                     <i className="bi bi-gear "></i>
                 </div>
-                <div className="col-lg-9 col-md-9 dashboard-right-profile float-start ">
+                <div
+                    className="col-lg-9 col-md-9 dashboard-right-profile float-start position-relative"
+                    onClick={toggleProfileDropdown}
+                >
                     <div className="float-start">
                         <img src="images/profile.png" alt="User Profile" />
                     </div>
-                    <div className="ms-3 float-start">
+                    <div className="ms-3 top-nav-profile-section float-start">
                         <h3>Asfak Mahmud</h3>
                         <p>asfakmahmudbd@gmaill.com</p>
+                        {isProfileDropdownVisible && <ProfileDropdown />}
                     </div>
                 </div>
             </div>
--- client/src/modules/platform/users/components/profile-dropdown.component.js
+++ client/src/modules/platform/users/components/profile-dropdown.component.js
@@ -3,8 +3,10 @@ import React from "react";
 const ProfileDropdown = () => {
     return (
         <div className="profile-dropdown-modal">
-            <ul className="list-unstyled profile-info mb-3">
-                <li><h2>John E. Grainger</h2></li>
+            <ul className="list-unstyled profile-info mb-2">
+                <li>
+                    <h2>John E. Grainger</h2>
+                </li>
                 <li>
                     <a>View my profile </a>
                 </li>
@@ -12,17 +14,25 @@ const ProfileDropdown = () => {
 
             <ul className="list-unstyled profile-edit">
                 <li>
-                  
-                    <a> <i className="bi bi-person-fill me-2"></i> Edit Profile </a>
+                    <a>
+                        {" "}
+                        <i className="bi bi-person-fill me-2"></i> Edit Profile{" "}
+                    </a>
                 </li>
                 <li>
-                    <a> <i className="bi bi-gear me-2"></i>Change Password </a>
+                    <a>
+                        {" "}
+                        <i className="bi bi-gear me-2"></i>Change Password{" "}
+                    </a>
                 </li>
             </ul>
 
             <ul className="list-unstyled profile-sign-out ">
                 <li>
-                    <a> <i className="bi bi-box-arrow-right me-2"></i>Sign out </a>
+                    <a>
+                        {" "}
+                        <i className="bi bi-box-arrow-right me-2"></i>Sign out{" "}
+                    </a>
                 </li>
             </ul>
         </div>
